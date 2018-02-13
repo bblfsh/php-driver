b37b30ab5bb16eac0f9b33c0f609d4fe0b5a1e62
Juanjo Alvarez <juanjo@sourced.tech>
2018-02-13 19:59:29 +0100

    Reworked the native types list, some annotations, more tests
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

--- driver/normalizer/annotation.go
+++ driver/normalizer/annotation.go
@@ -23,7 +23,32 @@ var Transformers = []transformer.Tranformer{
 // AnnotationRules describes how a UAST should be annotated with `uast.Role`.
 //
 // https://godoc.org/gopkg.in/bblfsh/sdk.v1/uast/ann
+
+var someAssignOp = On(Or(phpast.AssignOpPlus,
+                         phpast.AssignOpMinus,
+                         phpast.AssignOpMul,
+                         phpast.AssignOpDiv,
+                         phpast.AssignOpMod))
+
 var AnnotationRules = On(Any).Self(
 	On(Not(phpast.File)).Error(errors.New("root must be uast.File")),
-	On(phpast.File).Roles(uast.File, uast.Module).Descendants(),
+	On(phpast.File).Roles(uast.File, uast.Module).Descendants(
+		On(phpast.Alias).Roles(uast.Statement, uast.Alias),
+		On(phpast.Arg).Roles(uast.Argument),
+		On(phpast.Array).Roles(uast.Expression, uast.Literal, uast.List),
+		On(phpast.ArrayDimFetch).Roles(uast.Expression, uast.List, uast.Value, uast.Incomplete),
+		On(phpast.ArrayItem).Roles(uast.Expression, uast.List, uast.Value),
+
+		On(Or(phpast.Assign, someAssignOp).roles(uast.Expression, uast.Assignment).Roles(uast.Expression,
+			uast.Assignment).Children(
+				On(HasInternalRole("var").Roles(uast.Left),
+				On(HasInternalRole("expr").Roles(uast.Right),
+			),
+		),
+		On(phpast.AssignOpPlus).Roles(uast.Operator, uast.Add),
+		On(phpast.AssignOpMinus).Roles(uast.Operator, uast.Substract),
+		On(phpast.AssignOpMul).Roles(uast.Operator, uast.Multiply),
+		On(phpast.AssignOpDiv).Roles(uast.Operator, uast.Divide),
+		On(phpast.AssignOpMod).Roles(uast.Operator, uast.Modulo),
+	),
 )
---
+++ fixtures/cast.php
@@ -0,0 +1,11 @@
+<?php
+
+$a = (boolean)1;
+$b = (int)1;
+$c = (float)1;
+$d = (string)1;
+$e = (array)1;
+$f = (object)1;
+$g = (unset)1;
+$h = (binary)1;
+
